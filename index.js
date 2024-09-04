18810c82592a6a1ad5dc96f3789b19b05c953058
jayspatel <jayspatel2153@gmail.com>
2024-09-04 10:32:46 +0530

    JT-123 none

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
   app.use(express.json());
   
   app.get('/', (req, res) => {
-    res.send('Hello World.fddf..');
+    res.send('Hello World Devopss');
   });
 
   app.listen(port, () => {
