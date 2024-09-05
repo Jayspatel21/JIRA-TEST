6a91e2036b4c33844f21d1545d90bb0a8502b881
jayspatel <jayspatel2153@gmail.com>
2024-09-05 10:31:18 +0530

    JT-3 yml change

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more......fdgdgsfgdfgdfgdf.
+        Click on the Vite and React logos to learn more......stgae branch.
       </p>
     </>
   )
